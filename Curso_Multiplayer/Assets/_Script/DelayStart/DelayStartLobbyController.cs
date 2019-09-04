cbdcb6268c2828f12798220a1aaa7452142a6e19
Khalil Yamada Gangeme <khalil.yamada@hotmail.com>
2019-09-04 02:18:32 -0300

    Fim do vídeo 3
    ° Finalizado o terceiro vídeo do curso
     - Aqui foi criado um Lobby aonde os jogadores se encontram e esperam a partida ser iniciada
     - A sala tem um limite de 4 jogadores simultâneos
     - Caso o número mínimo não seja atingido o timer volta para 30 e fica parado lá
     - Caso o número máximo seja atingido o timer avança de onde estiver para contagem regressiva de 5 segundos
     - Quando o tempo acaba os jogadores são teletransportados para o jogo

---
+++ Curso_Multiplayer/Assets/_Script/DelayStart/DelayStartLobbyController.cs
@@ -0,0 +1,62 @@
+﻿using Photon.Pun;
+using Photon.Realtime;
+using UnityEngine;
+
+public class DelayStartLobbyController : MonoBehaviourPunCallbacks
+{
+
+
+    [SerializeField]
+    private GameObject delayStartButton; //Botão utilizado para criar e entrar em um jogo
+    [SerializeField]
+    private GameObject delayCancelButton; //Botão utilizado para parar de procurar uma sala de jogo
+    [SerializeField]
+    private int RoomSize; //Utilizado para setar manualmente o numero de jogadores de uma sala
+
+    public override void OnConnectedToMaster()
+    {
+        PhotonNetwork.AutomaticallySyncScene = true;
+        delayStartButton.SetActive(true);
+    }
+
+
+    public void DelayStart()
+    {
+        delayStartButton.SetActive(false);
+        delayCancelButton.SetActive(true);
+        PhotonNetwork.JoinRandomRoom();
+        Debug.Log("Delay start");
+    }
+
+    public override void OnJoinRandomFailed(short returnCode, string message)
+    {
+        Debug.Log("Failed to join a room");
+        CreateRoom();
+    }
+
+    void CreateRoom()
+    {
+        Debug.Log("Creating room now");
+        int randomRoomNumber = Random.Range(0, 10000);
+        RoomOptions roomOps = new RoomOptions() { IsVisible = true, IsOpen = true, MaxPlayers = (byte)RoomSize };
+        PhotonNetwork.CreateRoom("Room" + randomRoomNumber, roomOps);
+        Debug.Log(randomRoomNumber);
+    }
+
+    public override void OnCreateRoomFailed(short returnCode, string message)
+    {
+        Debug.Log("Failed to create a room... trying again");
+        CreateRoom();
+    }
+
+    public void DelayCancel()
+    {
+        delayCancelButton.SetActive(false);
+        delayStartButton.SetActive(true);
+        PhotonNetwork.LeaveRoom();
+    }
+
+
+
+}
+
